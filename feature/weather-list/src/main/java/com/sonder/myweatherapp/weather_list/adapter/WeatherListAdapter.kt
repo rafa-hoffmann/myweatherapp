b2e185a966cfb8a4adc08febe9defeb6f5d1a275
Rafael André Hoffmann <rafael.hoffmann1205@gmail.com>
2023-01-31 22:45:54 -0300

    add max and min temp

--- feature/weather-list/src/main/java/com/sonder/myweatherapp/weather_list/adapter/WeatherListAdapter.kt
+++ feature/weather-list/src/main/java/com/sonder/myweatherapp/weather_list/adapter/WeatherListAdapter.kt
@@ -12,6 +12,7 @@ import com.sonder.myweatherapp.feature.weather_list.databinding.ItemWeatherInfoB
 import com.sonder.myweatherapp.model.data.WeatherResource
 
 private const val IMAGE_SIZE_AND_EXTENSION = "@4x.png"
+
 class WeatherListAdapter :
     ListAdapter<WeatherResource, WeatherListAdapter.ViewHolder>(WeatherListAdapter) {
 
@@ -21,11 +22,25 @@ class WeatherListAdapter :
         fun bind(weather: WeatherResource) {
             with(binding) {
                 weatherTemperature.text = weather.main.temp.formatToShow()
+                weatherTemperatureMax.text = root.context.getString(
+                    R.string.weather_temperature_max,
+                    weather.main.tempMax.formatToShow()
+                )
+                weatherTemperatureMin.text = root.context.getString(
+                    R.string.weather_temperature_min,
+                    weather.main.tempMin.formatToShow()
+                )
                 weatherDescription.text = weather.weather.description
                 weatherFeelsLike.text =
-                    root.context.getString(R.string.weather_feels_like, weather.main.feelsLike.formatToShow())
+                    root.context.getString(
+                        R.string.weather_feels_like,
+                        weather.main.feelsLike.formatToShow()
+                    )
                 weatherHumidity.text =
-                    root.context.getString(R.string.weather_humidity, weather.main.humidity.formatToShow())
+                    root.context.getString(
+                        R.string.weather_humidity,
+                        weather.main.humidity.formatToShow()
+                    )
                 weatherLocation.text = weather.name
 
                 weatherImage.loadImage(weather.weather.icon + IMAGE_SIZE_AND_EXTENSION)
